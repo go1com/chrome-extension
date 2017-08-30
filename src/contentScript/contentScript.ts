530f08ae0d933e7e5e3c7685abfaa6f8215cd51f
Justin Chasez <justinchasez04121990@gmail.com>
2017-08-30 10:28:07 +0700

    WIP: do not execute chrome extension on go1 portal

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,26 +1,37 @@
 import {Go1ExtensionInjectionArea} from "./go1ExtensionInjectionArea";
 import {commandKeys} from "../commandHandlers/commandKeys";
+import * as _ from 'lodash';
 
 const chromeExtId = chrome.runtime.id;
 declare const $: any;
+const ignoringDomains = ['mygo1.com', 'go1.com'];
 
-$('head').append('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />')
+(function () {
+  const shouldIgnore = _.some(ignoringDomains, (domain) => window.location.href.indexOf(domain) > -1);
 
-Go1ExtensionInjectionArea.initialize();
-
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.name === commandKeys.checkQuickButtonSettings) {
-    Go1ExtensionInjectionArea.toggleQuickButton();
-    sendResponse({success: true});
-    return true;
+  if (shouldIgnore) {
+    console.log('ignoring go1 extension');
+    return;
   }
 
-  if (msg.name === commandKeys.checkCreateNoteSettings) {
-    Go1ExtensionInjectionArea.toggleCreateNote();
-    sendResponse({success: true});
-    return true;
-  }
+  $('head').append('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />');
 
-  sendResponse({success: false, message: 'No action handler found'});
-  return false;
-});
+  Go1ExtensionInjectionArea.initialize();
+
+  chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.name === commandKeys.checkQuickButtonSettings) {
+      Go1ExtensionInjectionArea.toggleQuickButton();
+      sendResponse({success: true});
+      return true;
+    }
+
+    if (msg.name === commandKeys.checkCreateNoteSettings) {
+      Go1ExtensionInjectionArea.toggleCreateNote();
+      sendResponse({success: true});
+      return true;
+    }
+
+    sendResponse({success: false, message: 'No action handler found'});
+    return false;
+  });
+})();
