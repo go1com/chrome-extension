4b2632d20eb1a09e7974bb6a2b7db1c516a49a71
Justin Chasez <justinchasez04121990@gmail.com>
2017-11-10 09:40:35 +0700

    [GCE-66] - Improve loading time for notes list page

--- src/modules/discussions/services/discussionNoFirebase.service.ts
+++ src/modules/discussions/services/discussionNoFirebase.service.ts
@@ -24,30 +24,6 @@ export class DiscussionNoFirebaseServiceService {
 
     queries.push(`type=${configuration.constants.noteChromeExtType}`);
 
-    if (!contextUrl && configuration.currentChromeTab && configuration.currentChromeTab.url) {
-      contextUrl = configuration.currentChromeTab.url
-    }
-
-    if (contextUrl) {
-      queries.push(`context[url]=${contextUrl}`);
-    }
-
-    if (queries.length) {
-      url += `?${queries.join('&')}`;
-    }
-
-    const response = await this.restClientService.get(url, await this.getCustomHeaders());
-    const publicNoteResponse = await this.getPublicNotesFromService(contextUrl);
-    // console.log(publicNoteResponse.concat(response));
-
-    return publicNoteResponse.concat(response);
-  }
-
-  async getPublicNotesFromService(contextUrl: string) {
-    let url = `${this.baseUrl}/${configuration.serviceUrls.noteService}notes`;
-
-    const queries = [];
-
     const currentPortalId = await this.storageService.retrieve(configuration.constants.localStorageKeys.currentActivePortalId);
 
     queries.push(`instance=${currentPortalId}`);
